25975e03ae59c03737efbd6aabf30d840c5e8bc0
selariousKnight <98381961+selariousKnight@users.noreply.github.com>
2022-08-12 22:52:55 +0530

    Update Guessing.js

--- Guessing.js
+++ Guessing.js
@@ -1,34 +1,49 @@
 let inputEle = document.getElementById("num");
 let buttonEle = document.getElementById("button");
 let attempts = 3;
-let ranNumber=Math.floor(Math.random()*100)
+let attemptsWarning = document.getElementById("attempts");
+let ranNumber=(Math.floor(Math.random()*10));
 let warningEle = document.getElementById("warning-para");
 buttonEle.addEventListener("click",function(){
-    if (inputEle.value === ""){
+    if (inputEle.value === "" || inputEle.value !== "number"){
         warningEle.textContent = "Please Enter number";
+        attempts-=1
+        attemptsWarning.textContent = `${attempts} Left`;
     }
     else if(inputEle.value == ranNumber){
-        warningEle.textContent="Congratulations your Guess is Correct";
-        warningEle.style.color = "green"
+        warningEle.textContent="Congratulations your Guess is Correct!!!";
+        warningEle.style.color = "green";
+        setTimeout(reloadPage,5000)
+        attemptsWarning.style.display = "none"
     }
     else if(inputEle.value < ranNumber){
-        warningEle.textContent = "Number is Bigger than your Guess";
         attempts-=1
+        warningEle.textContent = "Number is Bigger than your Guess";
         warningEle.style.color = "purple"
+        attemptsWarning.style.color = "RED";
+        attemptsWarning.textContent = `${attempts} Left`;
     }
     else if(inputEle.value > ranNumber){
-        warningEle.textContent = "Number is Smaller than your Guess";
         attempts-=1
+        warningEle.textContent = "Number is Smaller than your Guess";
         warningEle.style.color = "purple";
+        attemptsWarning.style.color = "RED";
+        attemptsWarning.textContent = `${attempts} Left`;
     }
-     if(attempts<=0){
-        warningEle.textContent = "Out of Chances Dear Better luck Next Time"
+    if(inputEle.value > 10){
+        warningEle.textContent = "Use Your damn Eyes Buddy";
+        warningEle.style.color = "RED";
+        attemptsWarning.textContent = `${attempts} Left`;
+        attemptsWarning.style.color = "RED";
+    }
+    if(attempts==0){
+        warningEle.textContent = "Out of Chances Dear Better luck Next Time";
         warningEle.style.color = "Red";
-        let timeOut = setTimeout(reloadPage,3000)
+        setTimeout(reloadPage,5000)
 
     }
-    
 })
+
 function reloadPage(){
     location.reload();
 }
